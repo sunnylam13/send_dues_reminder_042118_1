31480cb89f56c34df4c512aaa1fb127d13bb3f3a
Sunny Lam <sunny.lam@gmail.com>
2018-04-21 10:35:02 -0400

    revised code and added test data

--- send_dues/send_dues_reminder_042118_1.py
+++ send_dues/send_dues_reminder_042118_1.py
@@ -9,3 +9,27 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s")
 # logging.disable(logging.CRITICAL)
 
+import openpyxl,smtplib,sys
+
+# open the sheet and get latest due status
+
+wb = openpyxl.load_workbook('duesRecords.xlsx')
+logging.debug( 'Workbook opened' )
+
+sheet = wb['Sheet1']
+logging.debug( 'Sheet title is:  %s' % str(sheet.title) )
+
+lastCol = sheet.max_column
+logging.debug( 'Last column is:  %s' % str(lastCol) )
+
+latestMonth = sheet.cell(row=1,column=lastCol).value
+logging.debug( 'Latest month is:  %s' % str(latestMonth) )
+
+# check each member's payment status
+
+
+
+# log into email account
+
+# send out reminder emails
+
